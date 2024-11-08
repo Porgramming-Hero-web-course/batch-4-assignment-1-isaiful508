ddd3eea94748eee93977ee9d4886d02d8340be5e
Md. Saiful islam <isaiful.islam508@gmail.com>
2024-11-08 08:14:08 +0600

    problem 3 done

--- problem-3.ts
+++ problem-3.ts
@@ -1,10 +1,20 @@
 {
-//
-const countWordOccurrences = (sentence : string, word : string) => {
+    //
+    const countWordOccurrences = (sentence: string, word: string): number => {
 
-}
+        const convertSentenceLowerCase = sentence.toLocaleLowerCase();
+        const convertWordLowerCase = word.toLocaleLowerCase();
 
+        const words = convertSentenceLowerCase.split(" ");
 
-//
+        const count = words.filter(word => word === convertWordLowerCase).length;
+
+        return count;
+    }
+    const result = countWordOccurrences("I love typescript", "typescript");
+     
+    // console.log({result});
+
+    //
 }
 
